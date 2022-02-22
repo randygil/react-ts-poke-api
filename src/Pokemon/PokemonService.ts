d942b71a77a96ad47cfcf0af1182b6adb5e17c18
Randy Gil <r.gilcamejo@gmail.com>
2022-02-22 17:05:35 -0400

    add error handling

--- src/Pokemon/PokemonService.ts
+++ src/Pokemon/PokemonService.ts
@@ -2,23 +2,27 @@ import axios from "../http-common";
 import { IPokemonData } from "./types";
 export const findPokemon = async (
   pokemonName: string
-): Promise<IPokemonData> => {
-  const res = await axios.get(`/pokemon/${pokemonName}`);
+): Promise<IPokemonData | undefined> => {
+  try {
+    const res = await axios.get(`/pokemon/${pokemonName}`);
 
-  const {
-    name,
-    stats,
-    sprites: { front_default }
-  } = res.data;
-  const pokemonStats = stats.map((stat: any) => {
+    const {
+      name,
+      stats,
+      sprites: { front_default }
+    } = res.data;
+    const pokemonStats = stats.map((stat: any) => {
+      return {
+        name: stat.stat.name,
+        stat: stat.base_stat
+      };
+    });
     return {
-      name: stat.stat.name,
-      stat: stat.base_stat
+      name,
+      sprite_img: front_default,
+      stats: pokemonStats
     };
-  });
-  return {
-    name,
-    sprite_img: front_default,
-    stats: pokemonStats
-  };
+  } catch (error) {
+    alert(`Error: ${error.message}`);
+  }
 };
